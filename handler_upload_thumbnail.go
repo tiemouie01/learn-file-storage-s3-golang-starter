11b54fe2ee975b8f455e963e6f63b31d2d1d464e
Timothy Miamba <timal0361@gmail.com>
2025-02-09 09:51:16 +0200

    Implement thumbnail upload functionality

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,51 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// Get the media type and file data from the form data
+	mediaType := header.Header.Get("Content-Type")
+	fileData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to read file", err)
+		return
+	}
+
+	// Fetch the video to update
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to get video", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You are not authorized to upload a thumbnail for this video", nil)
+		return
+	}
+
+	// Create the thumbnail struct and update the video in the db
+	videoThumbnail := thumbnail{
+		data:      fileData,
+		mediaType: mediaType,
+	}
+	videoThumbnails[videoID] = videoThumbnail
+
+	thumbnailUrl := "http://localhost:" + cfg.port + "/api/thumbnails/" + videoID.String()
+	video.ThumbnailURL = &thumbnailUrl
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "There was an error updating the video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
